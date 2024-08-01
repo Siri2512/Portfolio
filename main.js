e49fcd0b5e94480a830ab1807e9196a605ec47a4
Siri2512 <balabhadrasaisiri@gmail.com>
2024-08-01 20:42:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed =new Typed(".text",{
+    strings: ["Python Developer","Data Structure Expert","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: false,
+    cursorChar: "|"
+});     
