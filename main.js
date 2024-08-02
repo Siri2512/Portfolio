2ef9aa7ffc7a3e81d9c421dab21fa6ae5a070fb6
Siri2512 <balabhadrasaisiri@gmail.com>
2024-08-02 22:57:31 +0530

    siri7

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var typed =new Typed(".text",{
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
 });
 
 // Change cursor size on hover
